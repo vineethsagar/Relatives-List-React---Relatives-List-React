0e271c13298b58d43afdc0a667fd121dbeb264bc
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-11-19 09:17:51 +0530

    added ordered list

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
-    }
+  render() {
+    let relatives = ["relative1", "relative2", "relative3"];
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key={"relativeListItem" + (index + 1)}>{relative}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
